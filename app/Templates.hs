ca1644d6a85113a16db384039bb459e641567786
derrickturk <dwt@terminusdatascience.com>
2017-10-20 18:03:39 -0500

    More fun with templating.

--- app/Templates.hs
+++ app/Templates.hs
@@ -1,14 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Templates where
+module Templates (
+    gameDescTemplate
+) where
 
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 
 import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe)
 
 import qualified Data.Text.Lazy as TL
 
+import Text.Numeral.Grammar (defaultInflection)
+import qualified Text.Numeral.Language.ENG as ENG
+
+pageTitle :: Int -> Html
+pageTitle n = h1 $ toHtml $
+  "The " <> fromMaybe plain fancy <> " Guy VOI Game" where
+    plain = TL.pack $ show n
+    fancy = (TL.toTitle . TL.fromStrict) <$> ENG.us_cardinal defaultInflection n
+
+intro :: Html
+intro = do
+  p "God flips a coin..."
+
 gameDescTemplate :: Int -> Html
 gameDescTemplate stages = do
-  p $ toHtml $ "a game with " <> TL.pack (show stages) <> " stages"
+  pageTitle stages
+  intro
+  p $ do
+    "a game with "
+    toHtml $ show stages
+    " stages"
